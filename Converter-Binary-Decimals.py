79f56bc6ffbfe71979c315bb411d93f22c0013f2
Bruno <br95st@protonmail.com>
2017-08-01 15:02:06 -0300

    Create Converter-Binary-Decimals.py

---
+++ Converter-Binary-Decimals.py
@@ -0,0 +1,11 @@
+numBin = []
+numDec = 0 
+print("Insira um valor em decimal")
+numDec = int(input())
+while numDec >= 1:
+    numBin.append(int(numDec % 2))
+    numDec = int(numDec / 2)
+numBin.reverse()
+print("binario: ", end="")
+for i in range(0, len(numBin), 1):
+    print(numBin[i], end="")
