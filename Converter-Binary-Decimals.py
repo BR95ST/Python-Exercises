efc7732778c70fd387f6db387681efee78c208f1
Bruno <br95st@protonmail.com>
2017-08-01 17:24:26 -0300

    Update Converter-Binary-Decimals.py

--- Converter-Binary-Decimals.py
+++ Converter-Binary-Decimals.py
@@ -6,6 +6,5 @@ while numDec >= 1:
     numBin.append(int(numDec % 2))
     numDec = int(numDec / 2)
 numBin.reverse()
-print("binario: ", end="")
 for i in range(0, len(numBin), 1):
     print(numBin[i], end="")
