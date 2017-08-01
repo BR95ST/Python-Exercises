f77c8ff9b214eb0d5e52d0bac2dd7fa5ce48b7b5
Bruno <br95st@protonmail.com>
2017-08-01 17:26:03 -0300

    Update Converter-Binary-Decimals.py

--- Converter-Binary-Decimals.py
+++ Converter-Binary-Decimals.py
@@ -3,8 +3,8 @@ numDec = 0
 print("Insira um valor em decimal")
 numDec = int(input())
 while numDec >= 1:
-    numBin.append(int(numDec % 2))
-    numDec = int(numDec / 2)
+    numBin.append(numDec % 2)
+    numDec = numDec / 2
 numBin.reverse()
 for i in range(0, len(numBin), 1):
     print(numBin[i], end="")
