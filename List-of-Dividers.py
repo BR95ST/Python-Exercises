15d381b0d01345566c274c3f2daf201163517c58
Bruno <br95st@protonmail.com>
2017-08-03 15:13:51 -0300

    Update List-of-Dividers.py

--- List-of-Dividers.py
+++ List-of-Dividers.py
@@ -1,12 +1,13 @@
 num = int(input())
 div = 2
-conjunto = [2]
+conjunto = []
 while num > 1:
     if num % div == 0:
         num = num/div
+        if div not in conjunto:
+            conjunto.append(div)
     else:
         div += 1        
-        conjunto.append(div)
 conjunto.sort()
 conjunto.reverse()
 print(conjunto)
