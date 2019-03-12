5b9ae626ec79ab4114744c47fc4ef0f3a94ad6e3
Bruno <br95st@gmail.com>
2019-03-11 22:25:49 -0300

    Add files via upload

---
+++ string_split.py
@@ -0,0 +1,7 @@
+"""Function receive a string and return a list with all characters of the string."""
+def string_split(string):
+	string = string
+	lista = []
+	for i in string:
+		lista.append(i)
+	return lista
